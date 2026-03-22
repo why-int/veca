6ffc939d6270081ffdf2bd68cf4a6d175e82534f
i.odegov <i.odegov@b-resh.ru>
2026-03-23 01:48:18 +0500

    init: инициализация проекта

--- src/core/app_logger.py
+++ src/core/app_logger.py
@@ -6,38 +6,45 @@ LOG_DIR = Path("logs")
 LOG_DIR.mkdir(exist_ok=True)
 
 # Основной лог-файл
-LOG_FILE = LOG_DIR / "app.log"
+LOG_FILE: Path = LOG_DIR / "app.log"
 
-def setup_logger(name: str = "velora", log_file: str = LOG_FILE, level=logging.DEBUG):
-    logger = logging.getLogger(name)
+
+def setup_logger(
+    name: str = "veca", log_file: Path = LOG_FILE, level=logging.DEBUG
+) -> logging.Logger:
+    logger = logging.getLogger(name=name)
     logger.setLevel(level)
 
     # Формат сообщений в логе
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
 
     # Консольный обработчик (StreamHandler)
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)  # в консоль выводим INFO и выше
-    ch.setFormatter(formatter)
+    ch.setLevel(level=logging.INFO)  # в консоль выводим INFO и выше
+    ch.setFormatter(fmt=formatter)
 
     # Файловый обработчик с ротацией по размеру (RotatingFileHandler)
     fh = logging.handlers.RotatingFileHandler(
-        log_file, maxBytes=5*1024*1024, backupCount=5, encoding='utf-8'
+        filename=log_file,
+        maxBytes=5 * 1024 * 1024,
+        backupCount=5,
+        encoding="utf-8",
     )
-    fh.setLevel(level)  # в файл пишем с уровнем level (обычно DEBUG)
-    fh.setFormatter(formatter)
+    fh.setLevel(level=level)  # в файл пишем с уровнем level (обычно DEBUG)
+    fh.setFormatter(fmt=formatter)
 
     # Добавляем обработчики, если они ещё не добавлены (чтобы избежать дублей)
     if not logger.hasHandlers():
-        logger.addHandler(ch)
-        logger.addHandler(fh)
+        logger.addHandler(hdlr=ch)
+        logger.addHandler(hdlr=fh)
 
     # Отключаем распространение логов вверх по иерархии (чтобы не дублировались)
     logger.propagate = False
 
     return logger
 
+
 # Инициализация логгера при импорте
-logger = setup_logger()
+logger: logging.Logger = setup_logger()
