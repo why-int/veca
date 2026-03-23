14bf0c8b0a88b364b240430dfc9d64e486921416
i.odegov <i.odegov@b-resh.ru>
2026-03-24 01:03:35 +0500

    WIP: main

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import asyncio
 
-from src2.core.app_context import app_context
-from src2.core.app_logger import logger
+from src.core.app_context import app_context
+from src.core.app_logger import logger
 
 
-async def main():
-    logger.info("Запуск приложения...")
+async def main() -> None:
+    logger.info(msg="Запуск приложения...")
     await app_context.setup()
     await app_context.dp.start_polling(app_context.bot)
-    logger.info("Polling завершён — бот остановлен")
+    logger.info(msg="Polling завершён — бот остановлен")
 
 
 if __name__ == "__main__":
--- src/config/base.py
+++ src/config/base.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -6,8 +7,5 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 class BaseConfig(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=BASE_DIR / ".env",
-        env_file_encoding="utf-8",
-        extra="ignore"
+        env_file=BASE_DIR / ".env", env_file_encoding="utf-8", extra="ignore"
     )
-
